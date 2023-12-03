fe8884228e25637595dc798b0f79fc60c68d0f3e
Hazem <h.kerwash@gmail.com>
2023-12-03 21:22:54 +0000

    Add + view article (WIP) + view + stored procedure

--- create_db.sql
+++ create_db.sql
@@ -19,13 +19,13 @@ CREATE TABLE users (
     username VARCHAR(20) NOT NULL UNIQUE,
     isEditor BOOLEAN DEFAULT FALSE,
     isAdmin BOOLEAN DEFAULT FALSE,
+    hashedPassword VARCHAR(128),
     PRIMARY KEY(user_id)
 );
 
 CREATE TABLE topics (
 	topic_id INT NOT NULL UNIQUE AUTO_INCREMENT,
     topic_title VARCHAR(20),
-    topic_description VARCHAR(100),
     PRIMARY KEY(topic_id)
 );
 
@@ -58,3 +58,44 @@ CREATE TABLE comments(
     FOREIGN KEY(user_id) REFERENCES users(user_id),
     FOREIGN KEY(article_id) REFERENCES articles(article_id)
 );
+
+DROP VIEW IF EXISTS view_articles;
+
+CREATE VIEW view_articles AS
+	SELECT a.article_id, a.article_date, a.article_title, u.username, t.topic_title, a.article_content
+    FROM articles a
+    JOIN topics t ON t.topic_id = a.topic_id
+    JOIN users u ON u.user_id = a.user_id
+    ORDER BY article_date;
+    
+DROP PROCEDURE IF EXISTS sp_insert_article;
+
+DELIMITER //
+CREATE PROCEDURE sp_insert_article(IN a_article_title VARCHAR(30), IN a_article_content MEDIUMTEXT, IN a_topic_title VARCHAR(20), IN a_username VARCHAR(20))
+BEGIN
+	DECLARE v_user_id INT;
+    DECLARE v_topic_id INT;
+    
+    SELECT user_id
+    FROM users
+    WHERE username = a_username
+    INTO v_user_id;
+    
+    IF ISNULL(v_user_id) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No matching username found';
+	END IF;
+    
+    SELECT topic_id
+    FROM topics
+    WHERE topic_title = a_topic_title
+    INTO v_topic_id;
+    
+    IF ISNULL(v_topic_id) THEN
+		INSERT INTO topics (topic_title) VALUES (a_topic_title);
+	END IF;
+    
+    INSERT INTO articles (article_date, article_title, article_content, user_id, topic_id)
+    VALUES (now(), a_article_title, a_article_content, v_user_id, (SELECT topic_id FROM topics WHERE topic_title = a_topic_title));
+    
+    END //
+    DELIMITER ;
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,5 @@
 module.exports = function(app, siteData) {
 
-    // Handle the routes
-
     app.get('/',function(req,res){
         res.render('index.ejs', siteData)
     });
@@ -13,4 +11,58 @@ module.exports = function(app, siteData) {
     app.get('/search',function(req,res){
         res.render("search.ejs", siteData);
     });
+
+    app.get('/writearticle',function(req,res){
+        let initialvalues = {username: '', topic: '', title: '', content: ''}
+
+        return renderAddNewArticle(res, initialvalues, "") 
+    });
+
+    function renderAddNewArticle(res, initialvalues, errormessage) {
+        let data = Object.assign({}, siteData, initialvalues, {errormessage:errormessage});
+        console.log(data)
+        res.render("writearticle.ejs", data);
+        return
+    }
+
+    app.post('/articleadded', function (req, res) {
+        let params = [req.body.title, req.body.content, req.body.topic, req.body.username]
+
+        let sqlquery = `CALL sp_insert_article('` + params[0] + `', '` + params[1] + `', '` + params[2] + `', '` + params[3] + `')`
+        db.query(sqlquery, params, (err, result) => {
+            if(err) {
+                return renderAddNewArticle(res, req.body, err.message)
+            }
+            res.send('Your article has been successfully posted!')
+        })
+    })
+
+    app.get('/viewarticles',function(req,res){
+
+        let sqlquery = `SELECT * FROM view_articles`;
+
+        db.query(sqlquery, (err, result) => {
+          if (err) {
+             res.redirect('./');
+          }
+
+          let data = Object.assign({}, siteData, {articles:result});
+          console.log(data)
+          res.render('viewarticles.ejs', data);
+        });
+    });
+
+    app.get('/clickedarticle', function(req,res){
+        let sqlquery = `SELECT * FROM view_articles WHERE article_title = '` + req.query.keyword + `'`;
+
+        db.query(sqlquery, (err, result) => {
+            if(err) {
+                res.redirect('./');
+            }
+
+            let data = Object.assign({}, siteData, {articles:result});
+            console.log(data)
+            res.render('clickedarticle.ejs', data);
+        })
+    })
 }
