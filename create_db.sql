d71d10e7ec38b7d6f42eabaeffbd0b6612102f1d
Hazem <h.kerwash@gmail.com>
2023-12-07 22:46:54 +0000

    Almost all functionalities done

--- create_db.sql
+++ create_db.sql
@@ -60,6 +60,9 @@ CREATE TABLE comments(
 );
 
 DROP VIEW IF EXISTS view_articles;
+DROP VIEW IF EXISTS search_articles;
+DROP VIEW IF EXISTS clicked_user;
+DROP VIEW IF EXISTS view_comments;
 
 CREATE VIEW view_articles AS
 	SELECT a.article_id, a.article_date, a.article_title, u.username, t.topic_title, a.article_content
@@ -67,14 +70,37 @@ CREATE VIEW view_articles AS
     JOIN topics t ON t.topic_id = a.topic_id
     JOIN users u ON u.user_id = a.user_id
     ORDER BY article_date;
+
+CREATE VIEW search_articles AS
+	SELECT a.article_id, a.article_date, a.article_title, a.article_content, t.topic_title, u.username
+    FROM articles a
+    JOIN topics t ON t.topic_id = a.topic_id
+    JOIN users u ON u.user_id = a.user_id;
+
+CREATE VIEW clicked_user AS
+	SELECT a.article_id, a.article_title, a.article_date, u.username
+    FROM users u
+    JOIN articles a ON u.user_id = a.user_id
+    ORDER BY article_date;
+
+CREATE VIEW view_comments AS
+	SELECT c.comment_date, c.comment_content, u.username, a.article_title
+    FROM comments c
+    JOIN users u ON u.user_id = c.user_id
+    JOIN articles a ON a.article_id = c.article_id;
     
 DROP PROCEDURE IF EXISTS sp_insert_article;
+DROP PROCEDURE IF EXISTS sp_edit_article;
+DROP PROCEDURE IF EXISTS sp_insert_comment;
 
 DELIMITER //
 CREATE PROCEDURE sp_insert_article(IN a_article_title VARCHAR(30), IN a_article_content MEDIUMTEXT, IN a_topic_title VARCHAR(20), IN a_username VARCHAR(20))
 BEGIN
 	DECLARE v_user_id INT;
     DECLARE v_topic_id INT;
+    DECLARE v_article_title VARCHAR(30);
+    DECLARE v_isEditor BOOLEAN;
+    DECLARE v_isAdmin BOOLEAN;
     
     SELECT user_id
     FROM users
@@ -94,8 +120,124 @@ BEGIN
 		INSERT INTO topics (topic_title) VALUES (a_topic_title);
 	END IF;
     
+    SELECT article_title
+    FROM articles
+    WHERE article_title = a_article_title
+    INTO v_article_title;
+    
+    IF NOT ISNULL(v_article_title) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Article title already exists';
+	END IF;
+    
+    SELECT isEditor
+    FROM users
+    WHERE username = a_username
+    INTO v_isEditor;
+    
+    SELECT isAdmin
+    FROM users
+    WHERE username = a_username
+    INTO v_isAdmin;
+    
+    IF NOT v_isEditor AND NOT v_isAdmin THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You do not have permission to post articles';
+	END IF;
+    
     INSERT INTO articles (article_date, article_title, article_content, user_id, topic_id)
     VALUES (now(), a_article_title, a_article_content, v_user_id, (SELECT topic_id FROM topics WHERE topic_title = a_topic_title));
     
-    END //
-    DELIMITER ;
+    INSERT INTO editorial (user_id, article_id)
+    VALUES (v_user_id, (SELECT article_id FROM articles WHERE article_title = a_article_title));
+    
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_edit_article(IN a_article_title VARCHAR(30), IN a_article_content MEDIUMTEXT, IN a_topic_title VARCHAR(20), IN a_username VARCHAR(20))
+BEGIN
+	DECLARE v_user_id INT;
+    DECLARE v_topic_id INT;
+    DECLARE v_article_id INT;
+    DECLARE v_article_title VARCHAR(30);
+	DECLARE v_isEditor BOOLEAN;
+    DECLARE v_isAdmin BOOLEAN;
+    DECLARE v_user_is_member INT;
+    
+	SELECT user_id
+    FROM users
+    WHERE username = a_username
+    INTO v_user_id;
+    
+    IF ISNULL(v_user_id) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No matching username found';
+	END IF;
+    
+    SELECT topic_id
+    FROM articles
+    WHERE article_title = a_article_title
+    INTO v_topic_id;
+        
+    UPDATE topics
+    SET topic_title = a_topic_title
+    WHERE topic_id = v_topic_id;
+    
+    SELECT article_id
+    FROM articles
+    WHERE article_title = a_article_title
+    INTO v_article_id;
+    
+    SELECT isEditor
+    FROM users
+    WHERE username = a_username
+    INTO v_isEditor;
+
+    SELECT isAdmin
+    FROM users
+    WHERE username = a_username
+    INTO v_isAdmin;
+    
+    IF NOT v_isEditor AND NOT v_isAdmin THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You do not have permission to edit articles';
+	END IF;
+    
+	SELECT COUNT(*) AS counteditorial
+    FROM editorial
+    WHERE user_id = v_user_id AND article_id = v_article_id
+    INTO v_user_is_member;
+    
+    IF v_user_is_member = 0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You do not have permission to edit this article';
+	END IF;
+    
+    UPDATE articles
+    SET article_content = a_article_content, user_id = v_user_id, article_date = now()
+    WHERE article_title = a_article_title;
+
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_insert_comment(IN c_comment_content MEDIUMTEXT, IN c_username VARCHAR(20), IN c_article_title VARCHAR(20))
+BEGIN
+	DECLARE v_user_id INT;
+    DECLARE v_article_id INT;
+    
+	SELECT user_id
+    FROM users
+    WHERE username = c_username
+    INTO v_user_id;
+    
+    IF ISNULL(v_user_id) THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No matching username found';
+	END IF;
+    
+	SELECT article_id
+    FROM articles
+    WHERE article_title = c_article_title
+    INTO v_article_id;
+    
+    INSERT INTO comments (comment_date, comment_content, user_id, article_id)
+    VALUES (now(), c_comment_content, v_user_id, v_article_id);
+
+END //
+DELIMITER ;
--- routes/main.js
+++ routes/main.js
@@ -12,6 +12,22 @@ module.exports = function(app, siteData) {
         res.render("search.ejs", siteData);
     });
 
+    app.get('/search-result', function (req, res) {
+
+        console.log(req.query)
+        let term = '%' + req.query.keyword + '%'
+        let sqlquery = `SELECT * FROM search_articles WHERE  article_title LIKE '` + term + `' OR topic_title LIKE '` + term + `' OR article_content LIKE '` + term + `'`
+
+        db.query(sqlquery, [term, term], (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+            let data = Object.assign({}, siteData, {articles:result});
+            console.log(data)
+            res.render('viewarticles.ejs', data)
+        });
+    });
+
     app.get('/writearticle',function(req,res){
         let initialvalues = {username: '', topic: '', title: '', content: ''}
 
@@ -37,6 +53,31 @@ module.exports = function(app, siteData) {
         })
     })
 
+    app.get('/editarticle',function(req,res){
+        let initialvalues = {username: req.query.username, topic: req.query.topic, title: req.query.title, content: req.query.content}
+
+        return renderEditArticle(res, initialvalues, "") 
+    });
+
+    function renderEditArticle(res, initialvalues, errormessage) {
+        let data = Object.assign({}, siteData, initialvalues, {errormessage:errormessage});
+        console.log(data)
+        res.render("editarticle.ejs", data);
+        return
+    }
+
+    app.post('/articleedited', function (req, res) {
+        let params = [req.body.title, req.body.content, req.body.topic, req.body.username]
+
+        let sqlquery = `CALL sp_edit_article('` + params[0] + `', '` + params[1] + `', '` + params[2] + `', '` + params[3] + `')`
+        db.query(sqlquery, params, (err, result) => {
+            if(err) {
+                return renderEditArticle(res, req.body, err.message)
+            }
+            res.send('The article has been successfully edited!')
+        })
+    })
+
     app.get('/viewarticles',function(req,res){
 
         let sqlquery = `SELECT * FROM view_articles`;
@@ -53,6 +94,7 @@ module.exports = function(app, siteData) {
     });
 
     app.get('/clickedarticle', function(req,res){
+
         let sqlquery = `SELECT * FROM view_articles WHERE article_title = '` + req.query.keyword + `'`;
 
         db.query(sqlquery, (err, result) => {
@@ -63,6 +105,72 @@ module.exports = function(app, siteData) {
             let data = Object.assign({}, siteData, {articles:result});
             console.log(data)
             res.render('clickedarticle.ejs', data);
+        })
+    })
+
+    app.get('/listusers',function(req,res){
+
+        let sqlquery = `SELECT * FROM users`
+                 
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+
+            let data = Object.assign({}, siteData, {users:result});
+            console.log(data)
+            res.render('listusers.ejs', data);
+        });                        
+    });
+
+    app.get('/clickeduser',function(req,res){
+
+        let sqlquery = `SELECT * FROM clicked_user WHERE username = '` + req.query.keyword + `'`
+                 
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+
+            let data = Object.assign({}, siteData, {users:result});
+            console.log(data)
+            res.render('clickeduser.ejs', data);
+        });                        
+    });
+
+    app.get('/addcomment', function(req, res){
+        let initialvalues = {username: '', content: '', article_title: req.query.keyword}
+
+        return renderAddNewComment(res, initialvalues, "")
+    });
+
+    function renderAddNewComment(res, initialvalues, errormessage) {
+        let data = Object.assign({}, siteData, initialvalues, {errormessage:errormessage});
+        console.log(data)
+        res.render("addcomment.ejs", data);
+        return
+    }
+
+    app.post('/commentadded', function(req, res) {
+        let params = [req.body.content, req.body.username, req.body.article_title]
+        let sqlquery = `CALL sp_insert_comment('` + params[0] + `', '` + params[1] + `', '` + params[2] + `')`
+        db.query(sqlquery, params, (err, result) => {
+            if(err) {
+                return renderAddNewComment(res, req.body, err.message)
+            }
+            res.send('Your comment has been added to the post')
+        })
+    })
+
+    app.get('/viewcomments', function(req, res) {
+        let sqlquery = `SELECT * FROM view_comments WHERE article_title = '` + req.query.keyword + `'`
+        db.query(sqlquery, (err, result) => {
+            if(err) {
+                res.redirect('./');
+            }
+            let data = Object.assign({}, siteData, {articles:result});
+            console.log(data)
+            res.render('viewcomments.ejs', data)
         })
     })
 }
